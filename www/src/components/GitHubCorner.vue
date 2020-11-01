00b35c22a78b3f6ddf91e5eeed00dd7a0b75d487
Manish Yadav <yadavmanish1607@gmail.com>
2020-11-01 02:56:27 -0500

    Correct vue config for custom domain

--- www/src/components/GitHubCorner.vue
+++ www/src/components/GitHubCorner.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- http://tholman.com/github-corners/  -->
   <a
-    href="https://github.com/alfg/ffprobe-web-analyzer"
+    href="https://github.com/meanmachin3/ffprobe-web-analyzer"
     class="github-corner"
     aria-label="View source on GitHub"
   >
--- www/vue.config.js
+++ www/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     }
   },
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/ffprobe-web-analyzer/'
+    ? '/'
     : '/',
   configureWebpack: {
     devServer: {
